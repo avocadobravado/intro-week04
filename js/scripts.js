61b034d7c766d80daf7be1f540819f87256b7de1
Rose Sponseller <rosesponseller@gmail.com>
2017-05-12 11:30:05 -0700

    Form inputs connect to JS in the console only

--- js/scripts.js
+++ js/scripts.js
@@ -3,47 +3,42 @@
 ////////////////////
 
 // Constructor for Pizza
-function Pizza(size, toppings, beverage) {
+function Pizza(size, topping, beverage) {
   this.size = size;
+  this.topping = topping;
   this.beverage = beverage;
-  this.toppings = [];
 }
 
 // Constructor for Customer information
-function Customer(name, address) {
-  this.name = name;
-  this.address = address;
-}
+// function Customer(name, address) {
+//   this.name = name;
+//   this.address = address;
+// }
+
+// Add Topping Array Prototype
+// Pizza.prototype.addToppingArray = function(topping) {
+//   this.addToppingArray.push(topping);
+// }
 
 // Prototype method to calculate price
-Pizza.prototype.calcPrice = function() {
-  var calcPrice = 7;
-
-  if (this.size === "Large") {
-    price *= 3;
-  } else if (this.size === "Medium") {
-    price *= 2;
-  } else {
-    price *= 1;
-  }
-
-  if (this.toppings.length === 0) {
-    price *= 1;
-  } else {
-    price += this.toppings.length;
-  }
-
-  return calcPrice;
-}
+// Pizza.prototype.calcPrice = function() {
+//   var calcPrice = 7;
+//
+//   if (this.size === "Large") {
+//     price *= 3;
+//   }
+//
+//   return calcPrice;
+// }
 
 // Prototype method to display pizza information
-Pizza.prototype.displayPizza = function() {
-  return "You have chosen a " + this.size + " pizza.";
-}
-// Prototype method to dipslay order details
-Customer.prototype.orderDetails = function() {
-  return "Thanks for placing your order, ";
-}
+// Pizza.prototype.displayPizza = function() {
+//   return "You have chosen a " + this.size + " pizza.";
+// }
+// // Prototype method to dipslay order details
+// Customer.prototype.orderDetails = function() {
+//   return "Thanks for placing your order, ";
+// }
 
 /////////////////////
 // Front end logic //
@@ -52,24 +47,20 @@ Customer.prototype.orderDetails = function() {
 $(function(){
   $("#order-form").submit(function(event) {
     event.preventDefault();
+
     var selectedPizzaSize = $("select#pizza-size").val();
-    var selectedToppings = $("input:checkbox[name=selected-topping]:checked").map(function(){
-        return this.value;
-    }).toArray();
+    var selectedToppings = [];
+    $("input:checkbox[name=selected-topping]:checked").each(function(){
+    selectedToppings.push($(this).val());
+    });
+    var selectedBeverage = $("select#beverage").val();
 
+    var newOrder = new Pizza(selectedPizzaSize, selectedToppings, selectedBeverage);
 
-    var newOrder = new Pizza(selectedPizzaSize, selectedToppings);
-
-    // $("input:checkbox[name=selected-topping]:checked").map(function(){
-    // selectedToppings.push($(this.toppings).val());
-    // });
-
-
-    var selectedToppings = $("input:checkbox[name=selected-topping]:checked").map(function(){
-        return this.value;
-    }).toArray();
-console.log(selectedToppings);
-console.log(newOrder);
+    console.log(selectedToppings);
+    console.log(newOrder);
+    console.log(newOrder.toppings);
+    console.log(newOrder.displayPizza);
 
 
   });
