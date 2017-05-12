99b9b9ca50d370754490f2c52990d601dc4a84f7
Rose Sponseller <rosesponseller@gmail.com>
2017-05-12 09:09:34 -0700

    Begin to add business and front end logic, including constructors and barebones info for prototypes

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+////////////////////
+// Back end logic //
+////////////////////
+
+// Constructor for pizza object
+function Pizza(size, toppings, beverage) {
+  this.size = size;
+  this.beverage = beverage;
+  this.toppings = [];
+}
+
+// Constructor for customer information
+function Customer(name, address) {
+  this.name = name;
+  this.address = address;
+}
+
+// Prototype method to calculate price
+Pizza.prototype.calcPrice = function() {
+  var total = 5;
+}
+
+// Prototype for order details
+Customer.prototype.orderDetails = function() {
+  return "Thanks for placing your order, ";
+}
+
+/////////////////////
+// Front end logic //
+/////////////////////
+
+$(function(){
+  $("#order-form").submit(function(event) {
+    event.preventDefault();
+
+
+
+
+  });
+});
